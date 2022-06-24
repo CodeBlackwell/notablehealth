0fe76d219e58ff5ec53845fca1d42b374d5422ab
codeblackwell <codeblackwell@gmail.com>
2022-06-23 17:23:25 -0700

    update, delete appointment. appointment time validation

--- routes/doctors.js
+++ routes/doctors.js
@@ -24,44 +24,81 @@ const getDoctorsNames = async (req, res)=> {
         }
         else {
             res.status(200).json(doctorNames)
+            return doctorNames
         }
     })
 }
 
+const createAppointments = async (req, res, records) => {
+    const validMinutes = {"0": True, "15": True, "30": True, "45": True}
+    for(let x = 0; x < records.length; x++) {
+        let convertedDate = new Date(records[x].fields.Date)
+        if (!validMinutes[convertedDate.getMinutes]){
+            res.status(400).send("Appointments can only be made at 15 minute intervals (0, 15, 30, 45)") 
+            return
+        }
+    }
+    base('Appointments').create(records, function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach(function (record) {
+          console.log(record.getId());
+        });
+      });
 
-// const getDoctorById = async (id) => {
+    res.send(200)
+}
 
-// }
+const getAppointmentById = async (req, res, id) => {
+    let details = {};
+    const appointment = await appointmentsTable.find(id, (err, record) => {
+        if (err) { res.status(500).json(err); console.error(err); return; }
+         record.fields
+        console.log(details)
+        for(let key in record.fields){
+            details[key] = record.fields[key]
+        }
+    })
+    return details
+}
+
+const deleteAppointmentById = async (id) => {
+    try {
+        const table = base('Appointments')
+        const destroyedRecord = await table.destroy(id)
+    } catch(err) {
+        console.log(err)
+    }
+    
+
+}
+
+const getDoctorById = async (id) => {
+    return await physiciansTable.find(id)
+}
 router.get('/', (req, res) => {
     getDoctorsNames(req,res)
 })
 
 router.get('/:physician_id', async (req, res) => {
-    let appointments;
+    let appointments = [];
     
-    const findAppointmentRecordById = async (id) => {
-        await base('Appointment').find(id, (err, record) => {
-            if (err) { res.status(500).json(err); console.error(err); return; }
-            return record
-        })
-    }
-
-    const appointmentIdList = await base('Doctors').find(req.params.physician_id, async (err, physician_record) => {
-        if (err) { res.status(500).json(err); console.error(err); return; }
-        let appointment_ids = JSON.stringify(physician_record.fields.Appointments)
-        console.log(appointment_ids)
-        return appointment_ids
+    const doctor = await getDoctorById(req.params.physician_id)
+    const apts = doctor.fields.Appointments.map(async apt_id => {
+      let apt = await getAppointmentById(req, res, apt_id)
+      console.log(apt)
     })
-    console.log(appointmentIdList, "OOF")
-    // console.log(findAppointmentRecordById({recordId: 'recw1LExZnN41QryV'}, (err, res) => { console.log(res)}))
-    
-    
 })
-    // .put( (req, res) => {
-    //     res.send(`update user with id ${req.params.id}`)
-    // })
-    // .delete( (req, res) => {
-    //     res.send(`delete user with id ${req.params.id}`)
-    // })
+
+router.delete('/appointments/:appointment_id', (req, res) => {
+    deleteAppointmentById(req.params.appointment_id)
+    res.status(200).send(`Appointment: ${req.params.appointment_id} has been deleted`)
+})
+
+router.post('/appointments/:appointment_fields', (req, res) => {
+    createAppointments(req, res, req.params.appointment_fields)
+})
 
 module.exports = router
